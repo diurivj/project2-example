840b2db81a7ff088a354d8c34c63145885a0357a
diurivj <diurivj@icloud.com>
2019-07-08 10:20:14 -0500

    i changed the title again b*tch

--- app.js
+++ app.js
@@ -47,7 +47,8 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
 // default value for title local
-app.locals.title = 'Esto lo cambió Diego';
+//app.locals.title = 'Esto lo cambió Diego';
+app.locals.title = 'Nopooo, te dije que así era, sin punto y coma;'
 
 
 
